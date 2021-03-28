4e2549df930a0ed038ef016b546af424a4436b1f
Corey Earwood <corey@authenticblas.com>
2021-03-28 15:19:09 -0600

    Add documentation comment to requireContentTypeMiddleware function

--- middleware/require_content_type.js
+++ middleware/require_content_type.js
@@ -1,3 +1,7 @@
+/**
+ * 
+ * Express middleware to enforce the existance of a content-type header
+ */
 export const requireContentTypeMiddleware = (req, res, next) => {
     if (!req.headers['content-type']) {
         res.sendStatus(415);
