e3db76174abf4a9f3dd7ae956097b6efd32ecab8
John Ballentine <johnballentine@users.noreply.github.com>
2021-05-23 20:25:21 -0500

    Posting text and rendering it works.

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():  # Display index.html as default
-    return render_template('index.html')
+    if request.method == 'POST':
+        received_post = request.form['text_field']
+        return render_template('index.html', received_post=received_post)
+    else:
+        return render_template('index.html')
 
 
 if __name__ == '__main__':
