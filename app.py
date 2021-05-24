8d1778cd614707f841c7b763ebf2b51b32fe1a75
John Ballentine <johnballentine@users.noreply.github.com>
2021-05-24 08:01:25 -0500

    Added json decoding from POST textarea form submission.

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, json
 
 app = Flask(__name__)
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():  # Display index.html as default
     if request.method == 'POST':
-        received_post = request.form['text_field']
-        return render_template('index.html', received_post=received_post)
+        received_json = json.loads(request.form['text_field'])
+
+        if received_json['is_malicious'] == 'is_malicious':
+            return render_template('index.html', received_post=received_json, is_malicious='is_malicious')       # received_post = request.json['text_field'] )  # received_post=request.form['text_field'])
+        else:
+            return render_template('index.html', received_post=received_json)
+
     else:
         return render_template('index.html')
 
