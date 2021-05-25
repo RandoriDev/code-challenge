004dc6dbd6415a4efd2bc8b65c453b0169cc67a2
John Ballentine <johnballentine@users.noreply.github.com>
2021-05-24 22:00:37 -0500

    Added request log database.

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
 from flask import Flask, render_template, request, json, abort
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 import requests
 
 app = Flask(__name__)
+
+# Request Log Database
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/logs.sqlite'
 db = SQLAlchemy(app)
+
+
+class RequestLog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    request_text = db.Column(db.Text, unique=True, nullable=False)
+    ip_address = db.Column(db.String(20))
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __init__(self, request_text, ip_address, timestamp):
+        self.request_text = request_text
+        self.ip_address = ip_address
+        self.timestamp = timestamp
+
+    def __repr__(self):
+        return "(Request Text: %s, IP Address: %s, Timestamp: %s)" \
+               % (self.request_text, self.ip_address, self.timestamp)
 
 # Credentials for backend REST API (no authentication required)
 API_URL = 'https://4rxx4pwar8.execute-api.us-east-1.amazonaws.com/live/code-challenge'
